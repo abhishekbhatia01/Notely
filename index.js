016d24b6177bd8a2c7626d246f93d1a9be8aa87a
abhishekbhatia01 <145684624+abhishekbhatia01@users.noreply.github.com>
2025-03-09 10:27:56 +0530

    New files addes

---
+++ index.js
@@ -0,0 +1,58 @@
+    const express = require('express');
+    const app = express();
+    const path = require('path');
+    const fs = require('fs');
+
+    app.set("view engine","ejs");
+    app.use(express.json());
+    app.use(express.urlencoded({extended : true}));
+    app.use(express.static(path.join(__dirname,'public')));
+    const taskFile = path.join(__dirname,"task.json");
+
+
+    app.get('/', (req,res)=>{
+        setTimeout(()=>{
+            res.render("index");
+        },2000);
+    });
+
+
+    let tasks = [];
+    if(fs.existsSync(taskFile)){
+        const data = fs.readFileSync(taskFile, 'utf-8');
+        if(data){
+            tasks = JSON.parse(data);
+        }
+    }
+    app.get('/task', (req, res) => {
+        setTimeout(() => {
+            res.render("notes"); 
+        }, 1000); 
+    });
+    
+    app.post('/tasks', (req,res) =>{
+        const {date, title, description} = req.body;
+
+        let task_id;
+
+        if(tasks.length == 0){
+            task_id = 1;
+        }else{
+            task_id = tasks[tasks.length-1].id+1;
+        }
+
+        const new_task = {
+            id : task_id,
+            date : new Date().toLocaleString(),
+            title : req.body.title,
+            description : req.body.description
+        }
+
+        tasks.push(new_task);
+        console.log(tasks);
+        fs.writeFileSync(taskFile, JSON.stringify(tasks, null, 2));
+        res.redirect('/task');
+    });
+
+
+    app.listen(2004);
